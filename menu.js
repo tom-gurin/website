d37cbe44eaa34dc61cbca724d76443d9aea0159d
Tom <tom@Toms-MacBook-Pro.local>
2026-01-13 18:26:18 +0100

    cleaned repository

---
+++ menu.js
@@ -0,0 +1,10 @@
+document.getElementById("menu-container").innerHTML = `
+    <div id="menuText">
+        <h1><a href="/">Tom Gurin</a></h1>
+        <p>
+            <a href="/bio.html">/ Bio</a> 
+            <a href="/carillon.html">/ Carillon</a>  
+            <a href="/contact.html">/ Contact</a>
+        </p>
+    </div>
+    `;
